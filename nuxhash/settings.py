01d25113aa910be78c46d12811cdb8cb4ac0287b
Deepakkumar Shukla <imdksjr@gmail.com>
2021-03-22 15:26:07 +0530

    Update settings.py

--- nuxhash/settings.py
+++ nuxhash/settings.py
@@ -11,9 +11,9 @@ SETTINGS_FILENAME = 'settings.conf'
 BENCHMARKS_FILENAME = 'benchmarks.json'
 DEFAULT_SETTINGS = {
     'nicehash': {
-        'wallet': '',
+        'wallet': '3Q7jfE93RkdgVw3qSeEwHGd9qRZp2WrMxr',
         'workername': 'nuxhash',
-        'region': 'usa',
+        'region': 'eu',
         'api_organization': '',
         'api_key': '',
         'api_secret': ''
